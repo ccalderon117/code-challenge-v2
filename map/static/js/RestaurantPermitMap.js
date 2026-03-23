708cd9d38e5de684039241e7fc5bc9d8aa5cdf17
Cesar Calderon <51422577+ccalderon117@users.noreply.github.com>
2026-03-22 22:19:02 -0500

    Complete DataMade code challenge 2026

--- map/static/js/RestaurantPermitMap.js
+++ map/static/js/RestaurantPermitMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet"
 
@@ -6,19 +6,9 @@ import "leaflet/dist/leaflet.css"
 
 import RAW_COMMUNITY_AREAS from "../../../data/raw/community-areas.geojson"
 
-function YearSelect({ setFilterVal }) {
-  // Filter by the permit issue year for each restaurant
+function YearSelect({ filterVal, setFilterVal }) {
   const startYear = 2026
-  const years = [...Array(11).keys()].map((increment) => {
-    return startYear - increment
-  })
-  const options = years.map((year) => {
-    return (
-      <option value={year} key={year}>
-        {year}
-      </option>
-    )
-  })
+  const years = [...Array(11).keys()].map((increment) => startYear - increment)
 
   return (
     <>
@@ -28,9 +18,14 @@ function YearSelect({ setFilterVal }) {
       <select
         id="yearSelect"
         className="form-select form-select-lg mb-3"
-        onChange={(e) => setFilterVal(e.target.value)}
+        value={filterVal}
+        onChange={(e) => setFilterVal(Number(e.target.value))}
       >
-        {options}
+        {years.map((year) => (
+          <option value={year} key={year}>
+            {year}
+          </option>
+        ))}
       </select>
     </>
   )
@@ -45,52 +40,121 @@ export default function RestaurantPermitMap() {
   const yearlyDataEndpoint = `/map-data/?year=${year}`
 
   useEffect(() => {
-    fetch()
+    fetch(yearlyDataEndpoint)
       .then((res) => res.json())
       .then((data) => {
-        /**
-         * TODO: Fetch the data needed to supply to map with data
-         */
+        setCurrentYearData(data)
+        console.log(data)
+      })
+      .catch((err) => {
+        console.error("Failed to fetch map data:", err)
+        setCurrentYearData([])
       })
   }, [yearlyDataEndpoint])
 
+  const totalPermits = useMemo(() => {
+    return currentYearData.reduce((sum, area) => sum + area.num_permits, 0)
+  }, [currentYearData])
 
-  function getColor(percentageOfPermits) {
-    /**
-     * TODO: Use this function in setAreaInteraction to set a community 
-     * area's color using the communityAreaColors constant above
-     */
+  const maxNumPermits = useMemo(() => {
+    if (!currentYearData.length) return 0
+    return Math.max(...currentYearData.map((area) => area.num_permits))
+  }, [currentYearData])
+
+  const geoJsonData = useMemo(() => {
+    return {
+      ...RAW_COMMUNITY_AREAS,
+      features: RAW_COMMUNITY_AREAS.features.map((feature) => {
+        const areaId = Number(feature.properties.area_num_1)
+        const matchedArea = currentYearData.find(
+          (area) => Number(area.area_id) === areaId
+        )
+
+        return {
+          ...feature,
+          properties: {
+            ...feature.properties,
+            area_id: areaId,
+            name: matchedArea?.name || feature.properties.community,
+            num_permits: matchedArea?.num_permits || 0,
+          },
+        }
+      }),
+    }
+  }, [currentYearData])
+
+  function getColor(numPermits) {
+    if (totalPermits === 0 || numPermits === 0) {
+      return communityAreaColors[0]
+    }
+
+    const percentageOfPermits = numPermits / totalPermits
+
+    if (percentageOfPermits >= 0.1) return communityAreaColors[3]
+    if (percentageOfPermits >= 0.05) return communityAreaColors[2]
+    if (percentageOfPermits > 0) return communityAreaColors[1]
+
+    return communityAreaColors[0]
   }
 
   function setAreaInteraction(feature, layer) {
-    /**
-     * TODO: Use the methods below to:
-     * 1) Shade each community area according to what percentage of 
-     * permits were issued there in the selected year
-     * 2) On hover, display a popup with the community area's raw 
-     * permit count for the year
-     */
-    layer.setStyle()
-    layer.on("", () => {
-      layer.bindPopup("")
-      layer.openPopup()
+    const { name, num_permits } = feature.properties
+
+    layer.setStyle({
+      fillColor: getColor(num_permits),
+      fillOpacity: 0.7,
+      color: "#666",
+      weight: 1,
+      opacity: 1,
+    })
+
+    layer.bindPopup(`
+      <strong>${name}</strong><br />
+      Permits in ${year}: ${num_permits}
+    `)
+
+    layer.on({
+      mouseover: () => {
+        layer.setStyle({
+          weight: 2,
+          color: "#222",
+          fillOpacity: 0.9,
+        })
+        layer.openPopup()
+      },
+      mouseout: () => {
+        layer.setStyle({
+          fillColor: getColor(num_permits),
+          fillOpacity: 0.7,
+          color: "#666",
+          weight: 1,
+          opacity: 1,
+        })
+        layer.closePopup()
+      },
+      click: () => {
+        layer.openPopup()
+      },
     })
   }
 
   return (
     <>
       <YearSelect filterVal={year} setFilterVal={setYear} />
+
       <p className="fs-4">
-        Restaurant permits issued this year: {/* TODO: display this value */}
+        Restaurant permits issued this year: {totalPermits}
       </p>
+
       <p className="fs-4">
-        Maximum number of restaurant permits in a single area:
-        {/* TODO: display this value */}
+        Maximum number of restaurant permits in a single area: {maxNumPermits}
       </p>
+
       <MapContainer
         id="restaurant-map"
         center={[41.88, -87.62]}
         zoom={10}
+        style={{ height: "600px", width: "100%" }}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -98,9 +162,9 @@ export default function RestaurantPermitMap() {
         />
         {currentYearData.length > 0 ? (
           <GeoJSON
-            data={RAW_COMMUNITY_AREAS}
+            data={geoJsonData}
             onEachFeature={setAreaInteraction}
-            key={maxNumPermits}
+            key={year}
           />
         ) : null}
       </MapContainer>
